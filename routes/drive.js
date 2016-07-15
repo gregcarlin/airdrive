fba5bce1885a392cca7ec1e58ea2558cc4c389a6
Greg Carlin <gregcarlin1@gmail.com>
2016-07-15 10:58:22 -0400

    Working on upload process

--- routes/drive.js
+++ routes/drive.js
@@ -39,6 +39,38 @@ router.get('/', function(req, res) {
 router.post('/upload', function(req, res) {
   resumable.post(req, function(status, filename, originalFilename, identifier) {
     if (status === 'done') {
+      core.getDb(function(err, db) {
+        if (err) {
+          // TODO
+          return;
+        }
+
+        var filesystem = db.collection('filesystem');
+        filesystem.findOne({userId: req.userId}, function(err, files) {
+          if (err) {
+            // TODO
+            db.close();
+            return;
+          }
+
+          // TODO put file in correct directory
+          files.root.children[filename] = {
+            type: 'file',
+            visibility: 'private',
+            status: 'uploading'
+          }
+          filesystem.updateOne({userId: req.userId}, {'$set': {root: files.root}}, {upsert: false}, function(err) {
+            db.close();
+            if (err) {
+              // TODO
+              console.log('1', err);
+            }
+
+            // nothing else to do
+          });
+        });
+      });
+
       var filePath = path.join(config.temporary_directory, filename);
       resumable.write(identifier, fs.createWriteStream(filePath), {
         onDone: function() {
@@ -49,13 +81,40 @@ router.post('/upload', function(req, res) {
             }
 
             core.storj.storeFileInBucket('5787efd4e7caec094411af9b', token.token, filePath, function(err, data) {
+              console.log('data', data);
               if (err) {
                 // TODO handle
                 return;
               }
 
-              // TODO stuff
-              console.log('data', data);
+              core.getDb(function(err, db) {
+                if (err) {
+                  // TODO
+                  return;
+                }
+
+                var filesystem = db.collection('filesystem');
+                filesystem.findOne({userId: req.userId}, function(err, files) {
+                  if (err) {
+                    // TODO
+                    db.close();
+                    return;
+                  }
+
+                  // TODO put file in correct directory
+                  files.root.children[filename].status = 'uploaded';
+                  // TODO add pointer to storj record
+
+                  filesystem.updateOne({userId: req.userId}, {root: files.root}, {upsert: false}, function(err) {
+                    db.close();
+                    if (err) {
+                      // TODO
+                    }
+
+                    // nothing else to do
+                  });
+                });
+              });
             });
           });
         }
