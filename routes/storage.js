c75bfbfee135cf555242eee6ec6f076df919450e
Greg Carlin <gregcarlin1@gmail.com>
2016-07-15 10:33:25 -0400

    Load filesystem from db

--- routes/storage.js
+++ routes/storage.js
@@ -2,77 +2,27 @@
 
 var express = require('express');
 var router = new express.Router();
-var fs = require('fs');
 
-var files = {
-  type: 'directory',
-  visibility: 'private',
-  children: {
-    Products: {
-      type: 'directory',
-      visibility: 'private',
-      children: {
-        Prototype: {
-          type: 'directory',
-          visibility: 'private',
-          children: {
-            'Folder 1': {
-              type: 'directory',
-              visibility: 'private'
-            },
-            'Folder 2': {
-              type: 'directory',
-              visibility: 'public'
-            },
-            'Folder 3': {
-              type: 'directory',
-              visibility: 'private'
-            },
-            'Document 1': {
-              type: 'file',
-              visibility: 'public'
-            },
-            'Document 2.txt': {
-              type: 'file',
-              visibility: 'private',
-              data: 'Hello world'
-            },
-            'Document 3': {
-              type: 'file',
-              visibility: 'private'
-            }
-          }
-        }
-      }
-    },
-    Documents: {
-      type: 'directory',
-      visibility: 'private'
-    },
-    Photos: {
-      type: 'directory',
-      visibility: 'public',
-      children: {
-        'lenna.jpg': {
-          type: 'file',
-          visibility: 'public',
-          data: ('<img src="data:image/png;base64,'
-                 + new Buffer(fs.readFileSync('routes/Lenna.png'))
-                   .toString('base64')
-                 + '" />')
-        }
-      }
-    },
-    Videos: {
-      type: 'directory',
-      visibility: 'private'
-    }
-  }
-};
+var core = require('./core');
 
 router.get('/', function(req, res) {
-  // TODO get data for this user
-  res.json(files);
+  core.getDb(function(err, db) {
+    if (err) {
+      // TODO
+      return;
+    }
+
+    var filesystem = db.collection('filesystem');
+    filesystem.findOne({userId: req.userId}, function(err, files) {
+      db.close();
+      if (err) {
+        // TODO
+        return;
+      }
+
+      res.json(files.root);
+    });
+  });
 });
 
 module.exports = router;
